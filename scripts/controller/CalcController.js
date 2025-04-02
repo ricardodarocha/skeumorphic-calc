931f3472d98c7813f0f73dcb0982a2d7d45de141
ValdirJunior <valdirsouzavjr@outlook.com>
2025-04-02 09:40:14 -0300

    fix

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,9 +2,28 @@ class CalcController{
 
 constructor(){
 
-    this.displayCalc = "0";
-    this.dataAtual;
+    this._displayCalc = "0";
+    this._currentDate;
 
 }
 
+get displayCalc(){
+
+    return this._displayCalc;
+}
+
+set displayCalc(valor){
+
+    this._displayCalc = valor;
+}
+
+get currentDate(){
+
+    return this._currentDate;
+}
+
+set current_currentDate(valor){
+
+    this._currentDate = valor;
+}
 }
