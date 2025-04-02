011bd4ec6c3d3515b9f4c8016e9d84691a882433
ValdirJunior <valdirsouzavjr@outlook.com>
2025-04-02 16:27:39 -0300

    feat

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ constructor(){
     this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
 
 }
 
@@ -21,6 +22,38 @@ initialize(){
 
     }, 1000)
     
+}
+
+
+addEventListenerAll(element, events, fn){
+
+    events.split(' ').forEach(event =>{
+
+        element.addEventListener(event, fn, false );
+    });
+}
+
+initButtonsEvents(){
+
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+
+    buttons.forEach((btn, index)=>{
+
+        this.addEventListenerAll(btn, "click drag", e=>{
+
+            console.log(btn.className.baseVal.replace("btn-", ""));
+        });
+        
+
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+
+            btn.style.cursor = "pointer";
+
+        });
+
+    });
+
 }
 
 setDisplayDateTime(){
@@ -67,7 +100,7 @@ get currentDate(){
     return new Date;
 }
 
-set current_currentDate(value){
+set currentDate(value){
 
     this._currentDate = value;
 }
