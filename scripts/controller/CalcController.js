dcf98c16b16789ef40bd4f2cf358a7e99585d08a
ValdirJunior <valdirsouzavjr@outlook.com>
2025-04-03 10:49:06 -0300

    feat

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController{
 
 constructor(){
 
+    this._operation = [];
     this._locale = 'pt-BR';
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
@@ -33,6 +34,129 @@ addEventListenerAll(element, events, fn){
     });
 }
 
+clearAll(){
+
+this._operation = [];
+
+}
+
+clearEntry(){
+
+    this._operation.pop();
+
+}
+
+getLastOperation(){
+
+    return this._operation[this._operation.length-1];
+}
+
+setLastOperation(value){
+
+    this._operation[this._operation.length-1] = value;
+}
+
+isOperator(value){
+
+    return (['+', '-', '*', '/', '.'].indexOf(value) > -1);
+}
+
+addOperation(value){
+
+    if (isNaN(this.getLastOperation() )){
+        //String
+
+        if(this.isOperator(value)){
+            //trocar o operador
+            this.setLastOperation(value);
+
+        }else if(isNaN(value)){
+
+            console.log(value);
+
+        } else{
+
+            this._operation.push(value);
+        }
+
+    } else{
+        //Number
+       let newValue = this.getLastOperation().toString() + value.toString();
+       this.setLastOperation(parseInt(newValue));
+    }
+
+    console.log(this._operation);
+}
+
+setError(){
+
+    this.displayCalc = "Error";
+}
+
+execBtn(value) {
+
+    switch (value){
+
+        case 'ac':
+            this.clearAll();
+            break;
+
+        case 'ce':
+                    this.clearEntry();
+            break;
+
+         case 'soma':
+                    this.addOperation('+');
+            break;
+
+         case 'subtracao':
+                    this.addOperation('-');
+            break;
+
+        case 'divisao':
+                    this.addOperation('/');
+             break;
+
+        case 'multiplicacao':
+                    this.addOperation('*');
+             break;
+
+        case 'porcento':
+                    this.addOperation('%');
+            break;
+
+        case 'igual':
+                   
+            break;
+
+        case 'ponto':
+                    this.addOperation(".");
+            break;
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        
+        this.addOperation(parseInt(value));
+        break;
+
+
+        default:
+            this.setError();
+            break;
+    
+    }
+
+}
+
 initButtonsEvents(){
 
     let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -42,7 +166,10 @@ initButtonsEvents(){
 
         this.addEventListenerAll(btn, "click drag", e=>{
 
-            console.log(btn.className.baseVal.replace("btn-", ""));
+            let textBtn = btn.className.baseVal.replace("btn-", "");
+
+            this.execBtn(textBtn);
+
         });
         
 
@@ -99,7 +226,7 @@ get currentDate(){
 
     return new Date;
 }
-
+ 
 set currentDate(value){
 
     this._currentDate = value;
